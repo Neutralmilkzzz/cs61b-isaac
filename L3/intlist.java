64f679949d6741020ab8e55ad762877779da45fb
isaac <isaaczhao771@gmail.com>
2025-10-27 21:23:29 +0800

    我真的副了“

--- L3/intlist.java
+++ L3/intlist.java
@@ -7,10 +7,19 @@ public class intlist{
 		rest = r;
 
 	}
+	/* Return the size of the list using recursion */
+	public int size(){
+		if (rest == null){
+			return 1;
+		}
+		return 1 + this.rest.size()
+	}
 
 	public static void main(String[] args){
 		intlist L = new intlist(15, null);
 		L = new intlist(10, L);
 		L = new intlist(5, L);
+
+		System.out.println(L.size());
 	}
 }
